e1cb25d010a039e83522204f7f4e6712739ba460
a.urbanite <a.urbanite@gmail.com>
2022-12-14 22:51:05 +0100

    initial commit

---
+++ src/components/simpleStateTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>this hand has {{fingers}} fingers</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fingers: 5,
+      }
+    }
+  }
+</script>
