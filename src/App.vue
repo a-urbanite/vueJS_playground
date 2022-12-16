0a4103b99aa28d0cc5da9289b1b72c061d25adb1
a.urbanite <a.urbanite@gmail.com>
2022-12-16 23:07:38 +0100

    simple data driven lsit display

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <SimpleStateTest/>
+  <!-- <ToDo></ToDo> -->
+  <h1>To-Do List</h1>
+  <ul>
+    <li v-for="item in ToDoItems" :key="item.label">
+      <ToDoItem :label="item.label" :done="item.done"></ToDoItem>
+    </li>
+  </ul>
 </template>
 
 <script>
 import SimpleStateTest from './components/simpleStateTest.vue';
+// import ToDo from './components/ToDo.vue'
+import ToDoItem from './components/toDoItem.vue';
+// import uniqueId from 'lodash.uniqueid';
 
 export default {
   name: 'App',
   components: {
-    SimpleStateTest
-}
+    SimpleStateTest,
+    // ToDo,
+    ToDoItem
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { label: 'Learn Vue', done: false },
+        { label: 'Create a Vue project with the CLI', done: true },
+        { label: 'Have fun', done: true },
+        { label: 'Create a to-do list', done: false }
+      ]
+    };
+  }
 }
 </script>
 
---
+++ src/components/ToDo.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>{{testState}}</h1>
+  <p>{{testObj.a}}</p>
+  <p>{{testArr[0]}}</p>
+  <p>{{testArr2[0].head}}</p>
+  <p>{{toDos[0].text}}</p>
+  <ul id="toDoList" class="item-{{$index}}">
+    <li v-repeat="toDos">{{$index}} - {{ text }} {{ details }}</li>
+  </ul>
+  <button v-on:click="addLI()">add li</button>
+  <ul id="tags">
+  <li v-repeat="tags">
+    {{$value}}
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toDos: [
+        { text: "cook dinner", details: "pasta bolognese would be nice" },
+        { text: "feed the cat", details: "that little piece of shit" },
+      ],
+      testState: "hrloooo",
+      testObj: {a: 1, b: 2},
+      testArr: [1, 2, 3],
+      testArr2: [{head: "head", tail: "tail"}],
+      el: '#tags',
+      data: {
+        tags: ['JavaScript', 'MVVM', 'Vue.js']
+      }
+    };
+  },
+
+  methods: {
+    addLI() {
+      this.toDos.push({text: "another task", details: "the struggle never ends"});
+    },
+  },
+};
+</script>
